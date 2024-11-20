d647b1e16579edf740290e7671ee119d161cc6c8
Sanjaisiva N GSS <sanjai@gsstech.in>
2024-11-20 10:00:03 +0530

    after adding if logic

--- fileTesting.js
+++ fileTesting.js
@@ -1,1 +1,10 @@
 let fname = "Sumi";
+let code = [10,5,7,7,3,2,10,3,6,9,1,6], k = 0; 
+console.log(decrypt(code, k));
+function decrypt(code, k) {
+    let resultArray = [];
+
+    if(k === 0) {
+        return code.fill(0, 0, code.length);
+    }
+}
